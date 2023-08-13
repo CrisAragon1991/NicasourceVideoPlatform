6bec3423ac9969a77f6a2b8ab039fcf07ded3b37
Cris <cristianaragon32a@gmail.com>
2023-08-13 12:15:07 -0600

    - Fix primary key error

--- src/migrations/1691800070266-add-role-seeder.ts
+++ src/migrations/1691800070266-add-role-seeder.ts
@@ -4,8 +4,8 @@ export class AddRoleSeeder1691800070266 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
-        INSERT INTO public."role" ("createdAt", "updatedAt", "deleteDate", id, "name") VALUES('2023-08-11 18:30:12.217', '2023-08-11 18:30:12.217', NULL, 1, 'Student');
-        INSERT INTO public."role" ("createdAt", "updatedAt", "deleteDate", id, "name") VALUES('2023-08-11 18:30:12.223', '2023-08-11 18:30:12.223', NULL, 2, 'Teacher');
+        INSERT INTO public."role" ("createdAt", "updatedAt", "deleteDate", "name") VALUES('2023-08-11 18:30:12.217', '2023-08-11 18:30:12.217', NULL, 'Student');
+        INSERT INTO public."role" ("createdAt", "updatedAt", "deleteDate", "name") VALUES('2023-08-11 18:30:12.223', '2023-08-11 18:30:12.223', NULL, 'Teacher');
         `)
     }
 
